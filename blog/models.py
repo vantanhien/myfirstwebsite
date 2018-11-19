9e3cdb698ddf99b57e15bf0d4f436e5f5ce53496
Mac <mac@VanTanHien.local>
2018-11-19 11:27:59 +0700

    add a Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Blog(models.Model):
+    title= models.CharField(max_length=100)
+    pub_date= models.DateTimeField()
+    body=models.TextField(max_length=200)
+    image_blog=models.ImageField(upload_to='image/')
 
