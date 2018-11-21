bd2df67fa0e1c84b8b89b37143b83d93ac93ceaf
Mac <mac@VanTanHien.local>
2018-11-21 15:36:36 +0700

    Done blog and some stuffs

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Blog(models.Model):
     title= models.CharField(max_length=100)
     pub_date= models.DateTimeField()
-    body=models.TextField(max_length=200)
+    body=models.TextField(max_length=10000)
     image_blog=models.ImageField(upload_to='image/')
 
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:100]
+    def pub_date_round(self):
+        return self.pub_date.strftime('%b %e %Y')
